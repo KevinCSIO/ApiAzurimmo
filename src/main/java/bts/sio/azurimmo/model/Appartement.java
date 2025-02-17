31f8b58486b2cee79ca74ac24f99b0855e00888d
KevinCSIO <kevin.camus@sts-sio-caen.info>
2025-02-17 16:58:41 +0100

    ajout getter setter sur ID

--- src/main/java/bts/sio/azurimmo/model/Appartement.java
+++ src/main/java/bts/sio/azurimmo/model/Appartement.java
@@ -36,6 +36,14 @@ public class Appartement {
         this.numero = numero;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public float getSurface() {
         return surface;
     }
